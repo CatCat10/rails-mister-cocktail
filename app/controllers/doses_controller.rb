cb0e36151b1d222563b475b74daaf1041d223709
Catherine Spyropoulos <cath.spyropoulos@gmail.com>
2019-02-21 17:51:35 +0100

    rake cleared, delete function added

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,8 +7,17 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.save
-    redirect_to cocktail_path(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
   end
 
   def dose_params
